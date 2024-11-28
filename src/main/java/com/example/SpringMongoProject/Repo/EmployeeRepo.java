8da19752583483ab247cb5a52caf1810a231ddc2
Nishigandha Ekapure <nishigandhaekapure13@gmail.com>
2024-11-28 12:50:13 +0530

    first commit

--- src/main/java/com/example/SpringMongoProject/Repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringMongoProject.Repo;
-
-import com.example.SpringMongoProject.Entity.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends MongoRepository<Employee,String> {
-}
--- src/main/java/com/example/SpringMongoProject/Repo/ManagerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringMongoProject.Repo;
-
-import com.example.SpringMongoProject.Entity.Manager;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManagerRepo extends MongoRepository<Manager,String> {
-}
